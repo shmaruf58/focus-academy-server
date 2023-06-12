e336f3a0e567ba82d085cc7edc67dcdc11521c14
shmaruf58 <maruf414100masum@gmail.com>
2023-06-12 21:41:15 +0600

    user

--- index.js
+++ index.js
@@ -86,21 +86,16 @@ async function run() {
 
     app.post("/users", async (req, res) => {
       const user = req.body;
-      
       const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-
       if (existingUser) {
         return res.send({ message: "user already exists" });
       }
-
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    // security layer: verifyJWT
-    // email same
-    // check admin
+
     app.get("/users/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
 
